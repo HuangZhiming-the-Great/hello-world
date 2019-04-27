67b5cc86ab6d2eeb62b08fae2b1e801b979ba91b
HuangZhiming-the-Great <48479621+HuangZhiming-the-Great@users.noreply.github.com>
2019-04-27 22:07:54 +0800

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -3,11 +3,7 @@ public class Calculate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-double a=Math.pow(10, 10)/17;
-double b=a*17;
-System.out.println(a);
-System.out.print(b);
-
+System.out.print("hello world!");
 	}
 
 }
